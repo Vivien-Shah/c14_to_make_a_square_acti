a9da378c9520819465c8d3964ed3ef840470c426
Vivien-Shah <hfsthane7787@gmail.com>
2025-07-28 19:39:39 +0530

    Add files via upload

---
+++ c14_square_proj.py
@@ -0,0 +1,9 @@
+# To make a square using turtle
+'''import turtle
+turtle.Screen().bgcolor("Red")
+square = turtle.Turtle()
+for i in range (4):
+    turtle.speed(70)
+    square.forward(200)
+    square.right(90)
+turtle.done()'''
